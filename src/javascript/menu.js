8e03a80e969c6218f44cc0896237ca925a170a8d
ArmanKol <31764763+ArmanKol@users.noreply.github.com>
2018-03-24 21:23:45 +0100

    pagina's werkend gekregen met app-localstorage

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,17 +1,18 @@
 function opslaan(){
-  var list = [];
-  var naam = localStorage.getItem("naamKey");
-  var ingredienten = localStorage.getItem("ingredientenKey");
-  list.push(naam);
-  list.push(ingredienten);
-  alert(list)
-  return list;
+  var naamvalue = localStorage.getItem("naamKey");
+  var ingredientenvalue = localStorage.getItem("ingredientenKey");
+
+  alert(naamvalue + " met de ingrediënten: " + ingredientenvalue + " is succesvol toegevoegd.");
+  window.location.href= "../../index.html";
 }
 
 function verwijderen(){
   var naamGerecht = localStorage.getItem("naamKey");
   var naamIngredient = localStorage.getItem("ingredientenKey");
   var naamVerwijderen = localStorage.getItem("naamverwijderenKey");
+
+  naamGerechtConvert = naamGerecht.toLowerCase();
+  naamVerwijderenConvert = naamVerwijderen.toLowerCase();
 
   if(naamGerecht == naamVerwijderen){
     localStorage.removeItem("naamverwijderenKey");
@@ -23,4 +24,31 @@ function verwijderen(){
   }else{
     alert("De naam van het gerecht klopt niet");
   }
+}
+
+function wijzigenNaamcheck(){
+  var naamInput = localStorage.getItem("naamcheckwijzigenKey");
+  var naamStored = localStorage.getItem("naamKey");
+
+  naamInputConvert = naamInput.toLowerCase();
+  naamStoredConvert = naamStored.toLowerCase();
+  if(naamInputConvert != naamStoredConvert){
+    alert("Naam klopt niet");
+  }else{
+    window.location.href = "../../eigenschappenWijzigen_volgende.html";
+  }
+}
+
+function nieuwegegevensOpslaan(){
+  var nieuwnaamInput = localStorage.getItem("nieuwenaamKey");
+  var nieuwingredientInput = localStorage.getItem("nieuweingredientKey");
+
+  localStorage.setItem("naamKey", nieuwnaamInput);
+  localStorage.setItem("ingredientenKey", nieuwingredientInput);
+
+  localStorage.removeItem("naamcheckwijzigenKey");
+
+  alert("De gegevens zijn succesvol opgeslagen");
+  window.location.href= "../../index.html";
+
 }
